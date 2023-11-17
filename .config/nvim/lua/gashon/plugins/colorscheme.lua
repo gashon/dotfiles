4578eb28d98b0f75b2f688f16833dedcf15d5cb3
GashonHussein <gashon96@gmail.com>
2023-11-17 15:58:51 -0800

    refactor(nvim): migrate to lazynvim

--- .config/nvim/lua/gashon/plugins/colorscheme.lua
+++ .config/nvim/lua/gashon/plugins/colorscheme.lua
@@ -2,11 +2,22 @@ return {
 
 	{ "rose-pine/neovim", name = "rose-pine" },
 
+	{
+		"craftzdog/solarized-osaka.nvim",
+		lazy = true,
+		priority = 1000,
+		opts = function()
+			return {
+				transparent = true,
+			}
+		end,
+	},
+
 	-- Configure LazyVim to load gruvbox
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "rose-pine",
+			colorscheme = "solarized-osaka",
 		},
 	},
 }
