db8dba885246dcf0d862e9ffcb8aee75fe05a7fe
Gashon Hussein <gashon96@gmail.com>
2024-06-19 00:23:34 -0700

    feat: add sync_dotfiles script

---
+++ scripts/sync_dotfiles.sh
@@ -0,0 +1,17 @@
+# sync_dotfiles.sh 
+# sync dotfiles to git dir
+
+if [ $(basename $(pwd)) != "dotfiles" ]; then
+    echo "Please run this script in dotfiles directory"
+    exit 1
+fi
+
+target_dir=$(pwd)
+dirs=(.psqlrc .vimrc .config/kitty .config/nvim .zshrc )
+
+for dir in ${dirs[@]}; do
+   target_dir=$(dirname $dir)
+   rsync -av --delete --progress $HOME/$dir $(pwd)/$target_dir
+done
+
+# rsync -av --delete --progress ${dirs[@]} $target_dir
