06b6d12b7c6c556f4d375a25efec6231af7d73e6
gashon <gashon96@gmail.com>
2024-07-02 23:55:13 -0700

    update gitsigns add inline blame nvim config

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -2,6 +2,25 @@ return {
   "lewis6991/gitsigns.nvim",
   event = "VeryLazy",
   opts = {
+    signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+    numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+    linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+    word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+    watch_gitdir = {
+      follow_files = true,
+    },
+    auto_attach = true,
+    attach_to_untracked = false,
+    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+      delay = 1000,
+      ignore_whitespace = false,
+      virt_text_priority = 100,
+    },
+    update_debounce = 100,
+    current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
     signs = {
       add = { text = "▎" },
       change = { text = "▎" },
