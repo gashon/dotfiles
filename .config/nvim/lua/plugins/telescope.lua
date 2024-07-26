87d1b430b7feeb0f3c20e724d0d2d4d290caa129
gashon <gashon96@gmail.com>
2024-07-26 16:09:55 -0700

    chore: update nvim deps and lockfile

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -10,10 +10,6 @@ return {
         layout_strategy = "horizontal",
         layout_config = { prompt_position = "top" },
         sorting_strategy = "ascending",
-        winblend = 0,
-        mappings = {
-          n = {},
-        },
       },
       pickers = {
         diagnostics = {
