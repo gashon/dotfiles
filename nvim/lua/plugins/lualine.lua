3f00cc667bd30a15fd889cecfee4cefbd49f5f16
gashon <gashon96@gmail.com>
2024-11-20 20:34:06 -0800

    nvim: rmv lualine clutter

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -6,6 +6,20 @@ return {
       theme = "ayu_dark",
       icons_enabled = true,
     },
+    sections = {
+      lualine_a = {},
+      lualine_b = {}, 
+      lualine_c = {
+      lualine_c = {
+        {
+          "filename",
+          path = 1, -- 0 = filename, 1 = relative path, 2 = absolute path
+        },
+      },
+      lualine_x = {}, 
+      lualine_y = {},
+      lualine_z = {},
+    },
   },
   config = function(_, opts)
     require("lualine").setup(opts)
