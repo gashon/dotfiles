ed922f76bb5a13e80ef5685fb8a9f0442b579b4a
gashon <gashon96@gmail.com>
2025-02-20 12:17:11 -0800

    pdbrc: add relative error matching debug utils

--- .pdbrc.py
+++ .pdbrc.py
@@ -62,18 +62,62 @@ def ref(tensor):
         raise NotImplementedError("Only PyTorch tensors are supported")
 
 
-def compare(script_path: str):
-    import subprocess
-    import sys
+def compare_tensors(grad_hw, grad_ref):
+    if not use_torch:
+        raise NotImplementedError("Only PyTorch tensors are supported")
 
-    result = subprocess.run(
-        [sys.executable, script_path],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        universal_newlines=True,
-    )
+    abs_diff = torch.abs(grad_hw - grad_ref)
+    max_diff = torch.max(abs_diff).item()
+    median_diff = torch.median(abs_diff).item()
 
-    print(result.stdout)
+    # Calculate relative errors, avoiding division by zeroj
+    with torch.no_grad():
+        nonzero_mask = grad_ref != 0
+        relative_error = torch.zeros_like(grad_ref)
+        relative_error[nonzero_mask] = abs_diff[nonzero_mask] / torch.abs(
+            grad_ref[nonzero_mask]
+        )
+        median_relative_error = (
+            torch.median(relative_error[nonzero_mask]).item()
+            if torch.any(nonzero_mask)
+            else float("inf")
+        )
+        mean_relative_error = (
+            torch.mean(relative_error[nonzero_mask]).item()
+            if torch.any(nonzero_mask)
+            else float("inf")
+        )
+        print(f"{median_relative_error=}")
+        print(f"{mean_relative_error=}")
+
+    print(f"{max_diff=}")
+    print(f"{median_diff=}")
+
+
+def compare(hw_key="hw", ref_key="ref"):
+    if not use_torch:
+        raise NotImplementedError("Only PyTorch tensors are supported")
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    grad_hw = torch.load(f"/tmp/{hw_key}", map_location="cpu").to(device)
+    grad_ref = torch.load(f"/tmp/{ref_key}", map_location="cpu").to(device)
+
+    print()
+    print()
+    print(f"{grad_hw.device=}")
+    print(f"{grad_ref.device=}")
+    print()
+
+    print(f"{grad_hw.dtype=}")
+    print(f"{grad_ref.dtype=}")
+    print()
+
+    print(f"{grad_hw.shape=}")
+    print(f"{grad_ref.shape=}")
+    print()
+
+    compare_tensors(grad_hw, grad_ref)
 
 
 def _hash(x):
